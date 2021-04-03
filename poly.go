ea72dc6a424f1f20f2b5b77b679158a86f72611f
teawithsand <teawithsand@gmail.com>
2021-04-03 06:59:14 +0000

    Removed todo comment

--- poly.go
+++ poly.go
@@ -5,8 +5,6 @@ type PolySerializer interface {
 	PolySerialize(data interface{}) (res []byte, err error)
 }
 
-// TODO(teawithsand): cleanup return type here in type registry with additonal options
-
 // PolyDeserializer handles polymprphic deserialization without any external hints about type of data deserialized
 // provided by caller.
 //
