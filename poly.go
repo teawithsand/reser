827e3ac1f5479fc9b70520c73ea2ba7d05e8a81f
teawithsand <teawithsand@gmail.com>
2022-04-17 17:52:03 +0000

    Renamed functions from serialize* to marshal*

--- poly.go
+++ poly.go
@@ -2,29 +2,29 @@ package reser
 
 // PolyMarshaler handles polymorphic serialization without any external hints about type of data serialized.
 type PolyMarshaler[T any] interface {
-	PolySerialize(data T) (res []byte, err error)
+	PolyMarshal(data T) (res []byte, err error)
 }
 
 type PolyMarshalerFunc[T any] func(data T) (res []byte, err error)
 
-func (f PolyMarshalerFunc[T]) PolySerialize(data T) (res []byte, err error) {
+func (f PolyMarshalerFunc[T]) PolyMarshal(data T) (res []byte, err error) {
 	return f(data)
 }
 
-type AnyPolySerializer = PolyMarshaler[any]
+type AnyPolyMarshal = PolyMarshaler[any]
 
 // PolyUnmarshaler handles polymorphic deserialization without any external hints about type of data deserialized
 // provided by caller.
 //
 // Note: This deserializer always returns struct values rather than pointer values.
 type PolyUnmarshaler[T any] interface {
-	PolyDeserialize(data []byte) (res T, err error)
+	PolyUnmarshal(data []byte) (res T, err error)
 }
 
 type AnyPolyUnmarshaler = PolyUnmarshaler[any]
 
 type PolyUnmarshalerFunc[T any] func(data []byte) (res T, err error)
 
-func (f PolyUnmarshalerFunc[T]) PolyDeserialize(data []byte) (res T, err error) {
+func (f PolyUnmarshalerFunc[T]) PolyUnmarshal(data []byte) (res T, err error) {
 	return f(data)
 }
--- simple.go
+++ simple.go
@@ -1,25 +1,25 @@
 package reser
 
 // Marshaler, which fits most interfaces exposed by golang already.
-// For instance, json.Marshal with SerializerFunc.
+// For instance, json.Marshal with MarshalerFunc.
 type Marshaler interface {
-	Serialize(data interface{}) (res []byte, err error)
+	Marshal(data interface{}) (res []byte, err error)
 }
 
 type MarshalerFunc func(data interface{}) (res []byte, err error)
 
-func (f MarshalerFunc) Serialize(data interface{}) (res []byte, err error) {
+func (f MarshalerFunc) Marshal(data interface{}) (res []byte, err error) {
 	return f(data)
 }
 
 // Unmarshaler, which fits most interfaces exposed by golang already.
-// For instance, json.Unmarshal with SerializerFunc.
+// For instance, json.Unmarshal with MarshalerFunc.
 type Unmarshaler interface {
-	Deserialize(data []byte, dst interface{}) (err error)
+	Unmarshal(data []byte, dst interface{}) (err error)
 }
 
 type UnmarshalerFunc func(data []byte, dst interface{}) (err error)
 
-func (f UnmarshalerFunc) Deserialize(data []byte, dst interface{}) (err error) {
+func (f UnmarshalerFunc) Unmarshal(data []byte, dst interface{}) (err error) {
 	return f(data, dst)
 }
